ce05107cd18d8ee7a0a85f6e00573cd5387199e5
Noelle <noelle.r.irvin@gmail.com>
2018-06-06 09:21:52 -0400

    Setup for asn retrieval feature

--- swimServer/src/main/java/com/capstone/swimServer/Controller/Controller.java
+++ swimServer/src/main/java/com/capstone/swimServer/Controller/Controller.java
@@ -1,14 +1,22 @@
 package com.capstone.swimServer.Controller;
 
+import com.capstone.swimServer.Dao.AsnDao;
 import com.capstone.swimServer.Model.Asn;
 import com.capstone.swimServer.Service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class Controller {
     @Autowired
     private Service service;
+    private AsnDao asnDao;
 
 //    @RequestMapping("/")
 //    public @ResponseBody Iterable<Asn> getAsn(){
@@ -18,6 +26,12 @@ public class Controller {
     @RequestMapping(value = "/receiveAsn", method= RequestMethod.POST)
     public void insertAsn(@RequestBody Asn asn) {
         service.insertAsnSerial(asn);
+    }
+
+    @RequestMapping(value = "/serialTest", method= RequestMethod.POST)
+    public List<Map<String, Object>> insertAsn(@RequestBody String str) {
+        return asnDao.getSerial(str);
+
     }
 
 }
--- swimServer/src/main/java/com/capstone/swimServer/Dao/AsnDao.java
+++ swimServer/src/main/java/com/capstone/swimServer/Dao/AsnDao.java
@@ -2,26 +2,28 @@ package com.capstone.swimServer.Dao;
 
 import com.capstone.swimServer.Model.Asn;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class AsnDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    private final String getASNQuery= "SELECT * FROM warehouse";
-
+    private final String getASNQuery= "SELECT * FROM warehouse WHERE asn = ?";
     private final String insertAsn= "INSERT INTO warehouse VALUES(?,?,?,?,?,?)";
-
     private final String insertSerial= "INSERT INTO itemizedAsn VALUES(?,?,?)";
+    private final String getSerialQuery = "SELECT serial FROM itemizedAsn WHERE = ";
 
-    public List getASN(){
-        List<Asn> AsnList = jdbcTemplate.query(getASNQuery, new BeanPropertyRowMapper<>(Asn.class));
-        return AsnList;
-    }
+//    private final String updateDockDoorQuery = "UPDATE warehouse SET dockDoor = ? WHERE asn = ?";
+
+//    public List getASN(){
+//        List<Asn> AsnList = jdbcTemplate.query(getASNQuery, new BeanPropertyRowMapper<>(Asn.class));
+//        return AsnList;
+//    }
 
     public void insertAsn(Asn asn){
         jdbcTemplate.update(insertAsn, asn.getAsn(), asn.getVendorId(), asn.getExpectedArrivalDate(), asn.getExpectedArrivalTime(), "IN TRANSIT", null);
@@ -29,6 +31,17 @@ public class AsnDao {
 
     public void insertSerial(int asn, int serial){
         jdbcTemplate.update(insertSerial, asn, serial, null);
-
     }
+
+    //Find a better way than this to avoid potential injection
+    public Asn getAsn(String asn){
+        Asn asnList = jdbcTemplate.queryForObject(getASNQuery, new Object[] {asn}, Asn.class);
+        return asnList;
+    }
+
+    public List<Map<String, Object>> getSerial(String asn){
+        List<Map<String, Object>> serials = jdbcTemplate.queryForList(getSerialQuery, asn);
+        return serials;
+    }
+
 }
